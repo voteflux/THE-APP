c59bf09a14082bac311b0c291f0aff81482a966a
Max Kaye <m@xk.io>
2018-04-18 11:45:25 +1000

    fix env var name

--- packages/lambdas/bin/travis-test-deploy-dev.sh
+++ packages/lambdas/bin/travis-test-deploy-dev.sh
@@ -17,7 +17,7 @@ else
     # this feels so unsafe...
     mkdir -p ~/.aws
     echo "[default]" >> ~/.aws/credentials
-    echo "aws_access_key_id = $AWS_ACCESS_KEY" >> ~/.aws/credentials
+    echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
     echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
 
     set -x
