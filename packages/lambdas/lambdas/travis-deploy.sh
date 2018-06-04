b28c5fe362749600da4f63209448fd7eb486d919
Max Kaye <m@xk.io>
2018-06-04 23:03:03 +1000

    Travis deploys prod when merged into master

---
+++ packages/lambdas/lambdas/travis-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+deploy(){
+    stage="$1"
+    if [ -z "$stage" ]; then
+        stage="dev"
+    fi
+    AWS_PROFILE=flux-api sls deploy --stage "$stage"
+    exit $!
+}
+
+
+if [ "$TRAVIS" != "true" ]; then
+    deploy "$1"
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    deploy "dev"
+elif [ "$TRAVIS_BRANCH" != "master" ]; then
+    deploy "dev"
+else
+    deploy "prod"
+fi
