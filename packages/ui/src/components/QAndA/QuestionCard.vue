0b53522fcf67c9c67ba90ac1a5af215a1ac768bb
Max Kaye <m@xk.io>
2019-01-31 23:49:30 +1100

    Modify question show/hide + hide actions

--- packages/ui/src/components/QAndA/QuestionCard.vue
+++ packages/ui/src/components/QAndA/QuestionCard.vue
@@ -7,14 +7,16 @@
             </v-layout>
         </v-card-title>
         <v-card-text>
-            <h4>Question: {{ showQ ? '' : '(hidden)' }}</h4>
+            <h4>Question:
+                <v-btn small v-if="showQ" @click="showQ = !showQ">Hide</v-btn>
+                <v-btn small v-if="!showQ" @click="showQ = !showQ">Show</v-btn></h4>
             <p v-if="showQ" style="white-space: pre-line" class="b--light-gray ba br3 pa2">{{qDoc.question}}</p>
         </v-card-text>
-        <v-card-actions>
-            <v-btn flat color="orange" @click="showQ = !showQ">{{ showQ ? 'hide q' : 'show q' }}</v-btn>
-            <v-btn flat color="orange" @click="doReply()">Reply</v-btn>
-            <v-btn flat color="orange" @click="showThread()" :disabled="this.reply_ids.unwrapOrDefault([]).length === 0">View Thread ({{ this.reply_ids.map(rids => rids.length).unwrapOrDefault('...') }})</v-btn>
-        </v-card-actions>
+        <!--<v-card-actions>-->
+            <!--<v-btn flat color="orange" @click="showQ = !showQ">{{ showQ ? 'hide q' : 'show q' }}</v-btn>-->
+            <!--<v-btn flat color="orange" @click="doReply()">Reply</v-btn>-->
+            <!--<v-btn flat color="orange" @click="showThread()" :disabled="this.reply_ids.unwrapOrDefault([]).length === 0">View Thread ({{ this.reply_ids.map(rids => rids.length).unwrapOrDefault('...') }})</v-btn>-->
+        <!--</v-card-actions>-->
     </v-card>
 </template>
 
